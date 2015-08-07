08f6dd930d1bfd52c6ba878252b4f0a4f2ea1c46
Angela Radulescu <angelar@princeton.edu>
2015-08-07 13:29:48 -0400

    added exercise 2 script

--- Stan-BernBeta-Script-Ex2.R
+++ Stan-BernBeta-Script-Ex2.R
@@ -17,12 +17,15 @@ modelString = "
   data {
     int<lower=0> N ; // number of observations
     int y[N] ; // array of observations of size N
+    int K ; // concentration parameter 
   }
   parameters {
+    real<lower=0,upper=1> omega;
     real<lower=0,upper=1> theta ;
   }
   model {
-    theta ~ beta(1,1) ;
+    omega ~ beta(1,1);
+    theta ~ beta(omega*(K-2)+1,(1-omega)*(K-2)+1) ;
     y ~ bernoulli(theta) ; 
   }
 " # close quote for modelString
@@ -31,24 +34,26 @@ modelString = "
 stanDso <- stan_model( model_code=modelString ) 
 
 # Specify data:
-N = 50 ; z = 10
+N = 50 ; z = 10; K = 1;
 y = c(rep(1,z),rep(0,N-z))
 dataList = list(
   y = y ,
-  N = N 
+  N = N ,
+  K = K
 )
 
 # Generate posterior sample:
 stanFit <- sampling( object=stanDso , 
                      data = dataList , 
                      chains = 3 ,
-                     iter = 1000 
+                     iter = 5000 ,
+                     warmup = 4500
                     )
 
 # Make graphs:
 mcmcCoda = mcmc.list( lapply( 1:ncol(stanFit) , 
                               function(x) { mcmc(as.array(stanFit)[,x,]) } ) )
-diagMCMC( mcmcCoda , parName=c("theta") )
+diagMCMC( mcmcCoda , parName=c("omega") )
 # saveGraph(file=paste0(fileNameRoot,"Diag"),type="eps")
 
 #------------------------------------------------------------------------------
